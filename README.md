d28dac9b35634fc7a12b919627677d8b9b6600bb
Kinderfeld <industrialmachine2000@protonmail.com>
2024-11-07 13:15:49 +0300

    General update

--- README.md
+++ README.md
@@ -3,27 +3,35 @@ quickly harden your server and deploy services in few clicks.
 
 Having problems with theSuffocater? Read this article:
 
-    === Init Systems ===:
+    === Init Systems ===
         - This build uses sysvinit initialization system. If you use systemd,
           try 'the-suffocater-legacy'. Unfortunately we dont have openrc build yet.
         
-    === Notes for BSD users ===:
+    === Notes for BSD users ===
         - *NetBSD*: Ensure that pkgin is installed on NetBSD before running the script. If it’s not
           available, you may need to install it using the default package manager.
         - *OpenBSD*: Ensure the necessary packages are available; pfctl is typically included,
           but you might want to double-check if you need fail2ban.
     
-    === Common problems with modules ===:
+    === Common problems with modules ===
         - Check if "modules" directory exists && Check if "modules" directory has any .py files.
         - Reinstall theSuffocater from GitHub. Maybe you just have a broken install.
         - Dont change any configurations files and directories unless you know what you are doing.
     
-    === Current existing bugs ===:
+    === Current existing bugs ===
         idk
         mail me if you found any - industrialmachine2000@protonmail.com
 
 
-Developer? Read this article:
+Developer? Want to contribute? Read this article:
     
-    === theSuffocater repository navigation ===:
+    === theSuffocater repository navigation ===
         - (blahblahblah)
+
+    === Current goals ===
+        - Refactor this ugly piece of shit called README.md
+        - Add more really useful modules
+        - Make GUI version for idiots
+        - Make it support every other geenu slash lenox and not just debian
+        - Check how it works on BSD systems
+        - Become a better programmer
--- modules/vpn_server_setup.py
+++ modules/vpn_server_setup.py
@@ -12,16 +12,17 @@ Date: 18.10.2024
 """
 
 import subprocess
+from sys import exit
 from os import system
 from time import sleep
 
 
-def openvpn_server_setup() -> None:
+def openvpn_server_setup(init_system: str) -> None:
     system("clear")
 
     print("We are going to setup your server for OpenVPN.")
 
-    answer: str = input("\nAre you sure you want this? (y/n): ")
+    answer: str = input("\nAre you sure you want this? (y/n): ").lower()
     if answer in ["y", "yes"]:
         try:
             print("Updating the system...")
@@ -35,21 +36,42 @@ def openvpn_server_setup() -> None:
             print("Installing Curl...")
             subprocess.run(["apt", "install", "curl", "-y"], check=True, shell=True)
             sleep(1)
+            
+            print("Installing OpenVPN-Installer script...")
+            subprocess.run(["curl", "-LJO", "https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh"], check=True, shell=True)
+            subprocess.run(["chmod", "+x", "openvpn-install.sh"], check=True, shell=True)
+            subprocess.run(["./openvpn-install.sh"], check=True, shell=True)
+            sleep(1)
 
             print("Installing Iptables for future...")
             subprocess.run(["apt", "install", "iptables", "-y"], check=True, shell=True)
             sleep(1)
+            
+            print("Finishing installation...")
+            if init_system == "sysvinit"
+                subprocess.run(["service", "openvpn", "restart"], check=True, shell=True)
+            else:
+                subprocess.run(["systemctl", "restart", "openvpn"], check=True, shell=True)
+            sleep(1)
 
+            print("Looks like you are locked and loaded.\nNow, move your *.ovpn configuration file to your client and enjoy the freedom.")
         except subprocess.CalledProcessError as e:
             print(f"An error occured: {e}")
 
 
-def wireguard_server_setup() -> None:
+def wireguard_server_setup(init_system: str) -> None:
     system("clear")
-    ...
+    
+    print("We are going to setup your server for Wireguard.")
+    answer: str = input("\nAre you sure you want this? (y/n): ").lower()
+    if answer in ["y", "yes"]:
+        try:
+            ...
+        except subprocess.CalledProcessError as e:
+            print(f"An error occured: {e}")
 
 
-def outlinevpn_server_setup() -> None:
+def outlinevpn_server_setup(init_system: str) -> None:
     system("clear")
 
     print("We are going to setup your server for OutlineVPN.")
@@ -79,13 +101,35 @@ def outlinevpn_server_setup() -> None:
             print(f"An error occured: {e}")
 
 
+def detect_init_system():
+    try:
+        output = subprocess.check_output(["systemctl"], stderr=subprocess.STDOUT, text=True)
+        if "systemd" in output:
+            return "systemd"
+    except FileNotFoundError:
+        pass
+    except subprocess.CalledProcessError:
+        pass
+
+    try:
+        output = subprocess.check_output(["service", "--help"], stderr=subprocess.STDOUT, text=True)
+        if "Usage: service" in output:
+            return "sysvinit"
+    except subprocess.CalledProcessError:
+        pass
+
+    print("This module does not support your init system.")
+    return None
+
+
 def vpn_server_setup() -> None:
     system("clear")
 
+    init_system = detect_init_system()
     functions: dict = {
             "openvpn_server_setup": openvpn_server_setup,
             "wireguard_server_setup": wireguard_server_setup,
-            "outlinevpn_server_setup": outline_vpn_server_setup
+            "outlinevpn_server_setup": outlinevpn_server_setup
             }
 
     print("+---- VPN Server Setup ----+")
@@ -95,4 +139,4 @@ def vpn_server_setup() -> None:
 
     your_function: str = input("Enter function name (or anything to leave) >>> ").lower()
     if your_function in functions:
-        functions[your_function]()
+        functions[your_function](init_system)
