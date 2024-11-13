69a8e277d49783e3b128500372d25f8c68ddc7bd
Kinderfeld <industrialmachine2000@protonmail.com>
2024-11-13 11:55:47 +0300

    first script, bug fixing, general improvements

--- requirements.sh
+++ requirements.sh
@@ -3,178 +3,230 @@
 clear
 
 list_of_platforms() {
-    DISTRIBUTIONS=(
-        "void" "alpine" "gentoo" "dragora" "slackware"
-        "fedora" "opensuse" "redhat" "freebsd" "netbsd" "openbsd"
-        "arch" "artix" "manjaro" "hyperbola" "parabola"
-        "debian" "ubuntu" "mint" "lmde" "trisquel" "devuan"
-    )
-
-    echo "Supported Platforms:"
-    for ELEMENT in "${DISTRIBUTIONS[@]}"; do
-        echo " - $ELEMENT"
-    done
+	DISTRIBUTIONS=(
+		"void" "alpine" "gentoo" "dragora" "slackware"
+		"fedora" "opensuse" "redhat"
+		"freebsd" "netbsd" "openbsd"
+		"arch" "artix" "manjaro" "hyperbola" "parabola"
+		"debian" "ubuntu" "mint" "lmde" "trisquel" "devuan"
+	)
+	
+	echo "Supported Platforms:"
+	for ELEMENT in "${DISTRIBUTIONS[@]}"; do
+		echo " - $ELEMENT"
+	done
 }
 
 install_python_requirements() {
-    python3 -m venv pkgenv
-    source pkgenv/bin/activate
-    pip install -r python_requirements.txt
-    echo "Don't forget to 'source pkgenv/bin/activate' and you are good to go."
+	python3 -m venv pkgenv
+	source pkgenv/bin/activate
+	pip install -r python_requirements.txt
+	
+	echo "Don't forget to 'source pkgenv/bin/activate' and you are good to go."
 }
 
 install_debian_based() {
-    apt update && apt upgrade -y
-    apt install python3 python3-pip net-tools ufw iptables fail2ban openvpn nftables lsof -y
+	echo "Installing requirements..."
+	sleep 1
+	
+	apt update && apt full-upgrade -y
+	apt install python3 python3-pip
+	apt install net-tools ufw iptables fail2ban openvpn nftables lsof git -y
 }
 
 install_arch_based() {
-    pacman -Syu --noconfirm
-    pacman -S python3 python3-pip net-tools ufw iptables fail2ban openvpn nftables lsof --noconfirm
+	echo "Installing requirements..."
+	sleep 1
+
+	pacman -Syu --noconfirm
+	pacman -S python3 python3-pip --noconfirm
+	pacman -S net-tools ufw iptables fail2ban openvpn nftables lsof git --noconfirm
 }
 
 install_gentoo_based() {
-    emerge --sync
-    emerge -uDN @world
-    emerge dev-python/pip
-    echo "Gentoo setup is not fully implemented yet. Please install required packages manually."
+	echo "Installing requirements..."
+	sleep 1
+
+	emerge --sync
+	emerge -uDN @world
+	emerge dev-python/pip
+	
+	echo "Gentoo setup is not fully implemented yet. Install it yourself, you are gentoo user after all!"
 }
 
 install_alpine_based() {
-    apk update && apk upgrade
-    apk add python3 py3-pip net-tools ufw iptables fail2ban openvpn nftables lsof
+	echo "Installing requirements..."
+	sleep 1
+
+	apk update && apk upgrade
+	apk add python3 py3-pip
+	apk add net-tools ufw iptables fail2ban openvpn nftables lsof git
 }
 
 install_void_based() {
-    xbps-install -S
-    xbps-install python3 python3-pip net-tools ufw iptables fail2ban openvpn nftables lsof
+	echo "Installing requirements..."
+	sleep 1
+
+	xbps-install -Su
+	xbps-install python3 python3-pip
+	xbps-install net-tools ufw iptables fail2ban openvpn nftables lsof git
 }
 
 install_fedora_based() {
-    dnf update -y
-    dnf install python3 python3-pip net-tools firewalld fail2ban openvpn nftables lsof -y
+	echo "Installing requirements..."
+	sleep 1
+	
+	dnf update -y
+	dnf install python3 python3-pip net-tools firewalld fail2ban openvpn nftables lsof git -y
 }
 
 install_opensuse_based() {
-    zypper refresh
-    zypper install -y python3 python3-pip net-tools firewalld fail2ban openvpn nftables lsof
+	echo "Installing requirements..."
+	sleep 1
+	
+	zypper refresh
+	zypper install -y python3 python3-pip
+	zypper install -y net-tools firewalld fail2ban openvpn nftables lsof git
 }
 
 install_slackware_based() {
-    slackpkg update
-    slackpkg install python3 python3-pip net-tools iptables fail2ban openvpn nftables lsof
+	echo "Installing requirements..."
+	sleep 1
+
+	slackpkg update
+	slackpkg install python3 python3-pip
+	slackpkg install net-tools iptables fail2ban openvpn nftables lsof git
 }
 
 
 install_redhat_based() {
-    yum update -y
-    yum install python3 python3-pip net-tools firewalld fail2ban openvpn nftables lsof -y
+	echo "Installing requirements..."
+	sleep 1
+
+	yum update -y
+	yum install python3 python3-pip -y
+	yum install net-tools firewalld fail2ban openvpn nftables lsof git -y
 }
 
 install_freebsd_based() {
-    pkg update
-    pkg install -y python3 py37-pip net-tools iptables fail2ban openvpn nftables lsof
+	echo "Installing requirements..."
+	sleep 1
+
+	pkg update -y
+	pkg install -y python3 py37-pip
+	pkg install -y net-tools iptables fail2ban openvpn nftables lsof git
 }
 
 install_netbsd_based() {
-    pkgin update
-    pkgin install python37 py37-pip net-tools iptables fail2ban openvpn nftables lsof
+	echo "Installing requirements..."
+	sleep 1
+
+	pkgin update
+	pkgin install python37 py37-pip
+	pkgin install net-tools iptables fail2ban openvpn nftables lsof git
 }
 
 install_openbsd_based() {
-    pkg_add python3 py3-pip net-tools pfctl fail2ban openvpn nftables lsof
+	echo "Installing requirements..."
+	sleep 1
+
+	pkg_add python3 py3-pip
+	pkg_add net-tools pfctl fail2ban openvpn nftables lsof git
 }
 
 install_dragora_based() {
-    pkg add python3 python3-pip net-tools ufw iptables fail2ban openvpn lsof
+	echo "Installing requirements..."
+	sleep 1
+
+	pkg add python3 python3-pip
+	pkg add net-tools ufw iptables fail2ban openvpn lsof git
 }
 
 to_lowercase() {
-    echo "$1" | tr "[:upper:]" "[:lower:]"
+	echo "$1" | tr "[:upper:]" "[:lower:]"
 }
 
 main() {
-    echo -n "Enter the base of your GNU/Linux or BSD distribution (Or type 'list' to view supported platforms): "
-    read DISTRO
-
-    if [[ "$DISTRO" == "list" ]]; then
-        list_of_platforms
-        main
-        return
-    fi
-
-    echo "--------------------------------------------------------------------------------------"
-
-    DISTRO=$(to_lowercase "$DISTRO")
-
-    case "$DISTRO" in
-        *debian*|*ubuntu*|*mint*|*lmde*|*trisquel*|*devuan*)
-            install_debian_based
-            ;;
-        *arch*|*manjaro*|*hyperbola*|*parabola*|*artix*)
-            install_arch_based
-            ;;
-        *gentoo*)
-            install_gentoo_based
-            ;;
-        *alpine*)
-            install_alpine_based
-            ;;
-        *void*)
-            install_void_based
-            ;;
-        *fedora*)
-            install_fedora_based
-            ;;
-        *opensuse*)
-            install_opensuse_based
-            ;;
-        *slackware*)
-            install_slackware_based
-            ;;
-        *redhat*)
-            install_redhat_based
-            ;;
-        *freebsd*)
-            install_freebsd_based
-            ;;
-        *netbsd*)
-            install_netbsd_based
-            ;;
-        *openbsd*)
-            install_openbsd_based
-            ;;
-        *dragora*)
-            install_dragora_based
-            ;;
-        *)
-            echo "Error: Unsupported distribution '$DISTRO'."
-            exit 1
-            ;;
-    esac
-
-    sleep 1
-    clear
-    echo -n "Now we need to create a virtual environment for Python3 in $(pwd). Do you wish to proceed? (y/n): "
-    read ANSWER
-
-    ANSWER=$(to_lowercase "$ANSWER")
-
-    if [[ "$ANSWER" == "y" ]]; then
-        install_python_requirements
-    else
-        exit 0
-    fi
+	echo -n "Enter the base of your GNU/Linux or BSD distribution (Or type 'list' to view supported platforms): "
+	read DISTRO
+	
+	if [[ "$DISTRO" == "list" ]]; then
+		list_of_platforms
+		main
+		return
+	fi
+	
+	echo "--------------------------------------------------------------------------------------"
+	
+	DISTRO=$(to_lowercase "$DISTRO")
+	
+	case "$DISTRO" in
+		*debian*|*ubuntu*|*mint*|*lmde*|*trisquel*|*devuan*)
+			install_debian_based
+			;;
+		*arch*|*manjaro*|*hyperbola*|*parabola*|*artix*)
+			install_arch_based
+			;;
+		*gentoo*)
+			install_gentoo_based
+			;;
+		*alpine*)
+			install_alpine_based
+			;;
+		*void*)
+			install_void_based
+			;;
+		*fedora*)
+			install_fedora_based
+			;;
+		*opensuse*)
+			install_opensuse_based
+			;;
+		*slackware*)
+			install_slackware_based
+			;;
+		*redhat*)
+			install_redhat_based
+			;;
+		*freebsd*)
+			install_freebsd_based
+			;;
+		*netbsd*)
+			install_netbsd_based
+			;;
+		*openbsd*)
+			install_openbsd_based
+			;;
+		*dragora*)
+			install_dragora_based
+			;;
+		*)
+			echo "Error: Unsupported distribution '$DISTRO'."
+			exit 1
+			;;
+	esac
+	
+	sleep 1
+	clear
+	echo -n "Now we need to create a virtual environment for Python3 in $(pwd). Do you wish to proceed? (y/n): "
+	read ANSWER
+	
+	ANSWER=$(to_lowercase "$ANSWER")
+	
+	if [[ "$ANSWER" == "y" ]]; then
+		install_python_requirements
+	else
+		exit 0
+	fi
 }
 
 check_privileges() {
-    if [ "$(id -u)" -eq 0 ]; then
-        main
-    else
-        echo "This script requires root privileges to install packages."
-        exit 1
-    fi
+	if [ "$(id -u)" -eq 0 ]; then
+		main
+	else
+		echo "This script requires root privileges to install packages."
+		exit 1
+	fi
 }
 
 check_privileges
-
---
+++ scripts/install_latest_version.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+clear
+
+check_internet_connection() {
+	echo "Checking internet connection..."
+	if ping -c 1 "www.gnu.org" &>/dev/null; then
+		echo "[*] Internet connection: OK"
+	else
+		echo "[*] Error: No internet connection detected."
+		exit 1
+	fi
+	}
+
+check_git_installed() {
+	if ! command -v git &>/dev/null; then
+		echo "Error: Git is not installed."
+		exit 1
+	fi
+}
+
+install_latest_version() {
+	echo "Installing latest theSuffocater versioni..."
+	sleep 1
+	
+	git clone https://github.com/Kinderfeld/fear-thesuffocater
+}
+
+check_internet_connection
+check_git_installed
+install_latest_version
