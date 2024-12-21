809b9e2d8fecfa0ad493122d6365258a265892e7
W <psezar@proton.me>
2024-12-21 14:07:07 +0300

    new indian function

--- modules/ssh_management.py
+++ modules/ssh_management.py
@@ -8,7 +8,7 @@ Manages your OpenSSH. It can:
 GNU/Linux and BSD supported.
 
 Authors: iva,
-         zaw (ssh_logging)
+         zaw (ssh_logging, ssh_key_gen)
 Date: 03.07.2024
 ---------------------------------------
 """
@@ -26,9 +26,50 @@ except ModuleNotFoundError as import_error:
     exit(1)
 
 
-def key_management() -> None:
+def ssh_key_gen() -> None:
     system("clear")
-    ...
+        
+    key_type: str = input("[==>] Select key type (rsa, dsa, ecdsa, ed25519; enter for default - rsa): ")
+    key_size: str = input("Select key size (1024, 2048, 3072 for rsa/dsa; enter for default - 2048): ")
+    
+    if key_type == "":
+        key_type = "rsa"
+        
+    if key_size == "":
+        key_size = "2048"
+
+    valid_key_types = ["rsa", "dsa", "ecdsa", "ed25519"]
+    valid_key_sizes = {
+        "rsa": ["1024", "2048", "3072", "4096"],
+        "dsa": ["1024", "2048", "3072"],
+        "ecdsa": ["256", "384", "521"],
+        "ed25519": ["256"],
+    }
+
+    if key_type not in valid_key_types:
+        print(f"{RED}[!] Error: Invalid key type: {key_type}{RESET}")
+        print("Available types: rsa, dsa, ecdsa, ed25519.")
+        return
+    
+    if key_size not in valid_key_sizes.get(key_type, []):
+        print(f"{RED}[!] Error: Size {key_size} is not valid for key type {key_type}{RESET}")
+        print(f"Available key sizes for {key_type}: {', '.join(valid_key_sizes.get(key_type, []))}.")
+        return 
+    
+    key_name = f"~/.ssh/id_{key_type}"
+    key_file = os.path.expanduser(key_name)
+
+    command = ["ssh-keygen", "-t", key_type]
+    if key_type != "ed25519":
+        command += ["-b", key_size]
+    command += ["-f", key_file, "-N", ""]
+
+    try:
+        subprocess.run(command, check=True)
+        print(f"Your SSH-keys: {key_file} и {key_file}.pub")
+    except subprocess.CalledProcessError:
+        print("{RED}[!] Error: failed to create SSH-keys: {error}")
+        return
 
 
 def ssh_logging() -> None:
@@ -124,7 +165,8 @@ def ssh_management() -> None:
     functions: dict = {
         "safe_ssh_setup": safe_ssh_setup,
         "ssh_logging": ssh_logging,
-        "key_management": key_management
+        "key_management": key_management,
+        "ssh_key_gen": ssh_key_gen     
     }
 
     print("+---- SSH Management  ----+")
