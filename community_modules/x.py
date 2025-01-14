93f9e4cd06551828ca973f85a7928eea33c58092
Kinderfeld <industrialmachine2000@protonmail.com>
2025-01-14 18:00:14 +0300

    update

--- community_modules/x.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-def x() -> None:
-    print("hell eyaha")
-
-
-if __name__ == "__main__":
-    x()
--- server_modules/f.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-
-def f():
-    print("hell yeah")
-
-
-if __name__ == "__main__":
-    test()
